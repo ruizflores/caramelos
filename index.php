12189b963c242c44a716c007e3f8ef7730c07c56
ruiz flores <floresrui123@gmail.com>
2016-03-08 22:53:12 -0500

    caramelos

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title></title>
-	<!-- Latest compiled and minified CSS -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="public/css/bootstrap.css">
 
 </head>
@@ -13,73 +13,65 @@
 
 <div class="container">
     <nav class="navbar navbar-default" role="navigation">
-    	<div class="container-fluid">
-    		<!-- Brand and toggle get grouped for better mobile display -->
-    		<div class="navbar-header">
-    			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-    				<span class="sr-only">Toggle navigation</span>
-    				<span class="icon-bar"></span>
-    				<span class="icon-bar"></span>
-    				<span class="icon-bar"></span>
-    			</button>
-    			<a class="navbar-brand" href="#">Title</a>
-    		</div>
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">ORQUESTA FOLKLORICA: LOS CARAMELOS DE HUÁNUCO</a>
+            </div>
     
-    		<!-- Collect the nav links, forms, and other content for toggling -->
-    		<div class="collapse navbar-collapse navbar-ex1-collapse">
-    			<ul class="nav navbar-nav">
-    				<li class="active"><a href="#">Link</a></li>
-    				<li><a href="#">Link</a></li>
-    			</ul>
-    			<form class="navbar-form navbar-left" role="search">
-    				<div class="form-group">
-    					<input type="text" class="form-control" placeholder="Search">
-    				</div>
-    				<button type="submit" class="btn btn-default">Submit</button>
-    			</form>
-    			<ul class="nav navbar-nav navbar-right">
-    				<li><a href="#">Link</a></li>
-    				<li class="dropdown">
-    					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-    					<ul class="dropdown-menu">
-    						<li><a href="#">Action</a></li>
-    						<li><a href="#">Another action</a></li>
-    						<li><a href="#">Something else here</a></li>
-    						<li><a href="#">Separated link</a></li>
-    					</ul>
-    				</li>
-    			</ul>
-    		</div><!-- /.navbar-collapse -->
-    	</div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav">
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">AMENIZA TODO TIPO DE  ACONTECIMIENTOS: Fiestas Patronales, Matrimonios, Bautizos, Cumpleaños, Aniversarios,etc</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">CONTRATOS A LOS NÚMEROS<b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">962578291</a></li>
+                            <li><a href="#">988375479</a></li>
+                            <li><a href="#">930620881</a></li>
+                            <li><a href="#">963688276</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div>
     </nav>
     <!-- SLIDER-->
     <div id="carousel-id" class="carousel slide" data-ride="carousel">
-    	<ol class="carousel-indicators">
-    		<li data-target="#carousel-id" data-slide-to="0" class=""></li>
-    		<li data-target="#carousel-id" data-slide-to="1" class=""></li>
-    		<li data-target="#carousel-id" data-slide-to="2" class="active"></li>
-    	</ol>
-    	<div class="carousel-inner">
-    		<div class="item">
-    			<img data-src="public/imagenes/1.jpg" alt="First slide" src="public/imagenes/1.jpg">
-    			
-    		</div>
-    		<div class="item">
-    			<img data-src="public/imagenes/2.jpg" alt="Second slide" src="public/imagenes/2.jpg">
-    			
-    		</div>
-    		<div class="item active">
-    			<img data-src="public/imagenes/3.jpg" alt="Third slide" src="public/imagenes/3.jpg">
-    			
-    		</div>
-    	</div>
-    	<a class="left carousel-control" href="#carousel-id" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
-    	<a class="right carousel-control" href="#carousel-id" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+        <ol class="carousel-indicators">
+            <li data-target="#carousel-id" data-slide-to="0" class=""></li>
+            <li data-target="#carousel-id" data-slide-to="1" class=""></li>
+            <li data-target="#carousel-id" data-slide-to="2" class="active"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="item">
+                <img data-src="public/imagenes/11.jpg" alt="First slide" src="public/imagenes/11.jpg">
+                
+            </div>
+            <div class="item">
+                <img data-src="public/imagenes/12.jpg" alt="Second slide" src="public/imagenes/12.jpg">
+
+            </div>
+            <div class="item active">
+                <img data-src="public/imagenes/13.jpg" alt="Third slide" src="public/imagenes/13.jpg">
+                
+            </div>
+        </div>
+        <a class="left carousel-control" href="#carousel-id" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+        <a class="right carousel-control" href="#carousel-id" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
     </div>
 </div>
 
-	<!-- Latest compiled and minified JavaScript -->
-	<script src="public/js/jquery-1.12.1.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="public/js/jquery-1.12.1.js"></script>
 <script src="public/js/bootstrap.js"></script>
 
 
